da3b43d54b44baa651fbd0ec847bcc14c8764428
pgas <pierre.gaston@gmail.com>
2017-04-18 16:49:30 +0300

    some manually run tests

---
+++ tests/drawRootText.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# try possible locations
+for lib in libs/ ../libs/;do
+    if [[ -f $lib/newt.so ]];then
+	enable -f "$lib"/newt.so newt
+	break
+    fi
+done
+
+if ! type newt &>/dev/null;then
+    echo >&2 unable to load the new builtin
+fi
+set -x       
+newt draw
+newt draw roottext
+newt draw roottext 2
+newt draw roottext 4
+
+set +x
+
+newt init
+newt cls
+newt draw roottext 10 10 "some root text"
+newt refresh
+sleep 2
+newt finished
+
