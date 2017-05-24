81eb4ee4777e2e527297620fe2234c8223117f91
pgas <pierre.gaston@gmail.com>
2017-05-24 22:47:41 +0300

    small step

--- support/make_wrapper.sh
+++ support/make_wrapper.sh
@@ -1,6 +1,32 @@
 #!bin/bash
 
 
+
+
+# hashing using djb2...bash version
+# unsigned long
+#     hash(unsigned char *str)
+#     {
+#         unsigned long hash = 5381;
+#         int c;
+#         while (c = *str++)
+#             hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+#         return hash;
+#     }
+
+djb2() {
+     hash=5381
+     l=${1,,} # lowercase the words
+     for ((i=0;i<${#l};i+=1)); do
+ 	 LC_CTYPE=C printf -v c %d "'${l:i:1}"
+ 	 (( hash = ((hash '<<' 5) + hash)+c ))
+     done
+     printf -v hs " %ul" $hash
+     hash="${hs%l}ull"
+}
+
+
+
 extract_fn () {
 # another parsing attempt....
 # first line strips cpp, typedefs and comments
@@ -23,6 +49,10 @@ break_fn_name () {
     done
 }
 
+declare -A subcommands
+declare -A usage
+declare -A fdecls
+
 parse_fn () {
     paren_pos=0
     until [[ ${def[paren_pos]} =  "(" ]];do ((paren_pos+=1));done
@@ -30,10 +60,13 @@ parse_fn () {
     fret=${def[*]:0:paren_pos-2+1}
 #    echo >&2 "fname: $fname"
     break_fn_name "$fname"
-    fdecls+=("int $R(WORD_LIST * list);")
-
-    fdef=()
-    fdef+=("int $R(WORD_LIST * list); {" )
+    subco=${R%%_*}
+    subco=${subco#[![:alpha:]]}     subco=${subco#[![:alpha:]]}
+    fdecls[$subco]+=$'\n'"int $R(WORD_LIST * list);"
+    echo >&2 "subco $subco"
+    subcommands[$subco]=""
+    usage[$subco]+=$'\n'"	newt $subco"
+    fdef[$subco]+=$'\n'"int $R(WORD_LIST * list); {" 
     args=()
     cur_pos=paren_pos
     while [[ ${def[cur_pos]} !=  ")" ]];do
@@ -42,43 +75,46 @@ parse_fn () {
 	argtype=${def[@]:cur_pos+1:end_arg-cur_pos-2}
 	argname=${def[end_arg-1]}
 	args+="$argname, "
+	usage[$subco]+=" $argname"
 	#	echo  >&2 "argtype: $argtype, argname: $argname"
 	
 	case $argtype in
 	    int|unsigned\ int)
-		fdef+=("    READ_INT(list, $argname, ENTRY_USAGE);");;
+		fdef[$subco]+=$'\n'"    READ_INT(list, $argname, ${subco^^}_USAGE);";;
 	    *char*\*)
-		fdef+=("    READ_STRING(list, $argname, ENTRY_USAGE);");;
+		fdef[$subco]+=$'\n'"    READ_STRING(list, $argname, ${subco^^}_USAGE);";;
 	    newtComponent)
-		fdef+=("    READ_COMPONENT(list, $argname, ENTRY_USAGE);");;
+		fdef[$subco]+=$'\n'"    READ_COMPONENT(list, $argname, ${subco^^}_USAGE);";;
 	    '')
 		args=
 		;;
 	    *)
-		fdef+=("    /* TODO: read $argname of $argtype */") ;;
+		fdef[$subco]+=$'\n'"    /* TODO: read $argname of $argtype */" ;;
 	esac
 	cur_pos=$end_arg
     done
-    printf "%s\n" "${fdef[@]}" ""
+    fdef[$subco]+=$'\n'
     case $fret in
 	int)
-	    echo "    WRITE_INT(vname, $fname(${args%, });"
+	    fdef[$subco]+=$'\n'"    WRITE_INT(vname, $fname(${args%, });"
 	    ;;
 	*char*\*)
-	    echo "    WRITE_STRING(vname, $fname(${args%, });"
+	    fdef[$subco]+=$'\n'"    WRITE_STRING(vname, $fname(${args%, });"
 	    ;;
 	newtComponent)
-	    echo "    WRITE_COMPONENT(vname, $fname(${args%, });"
+	    fdef[$subco]+=$'\n'"    WRITE_COMPONENT(vname, $fname(${args%, });"
 	    ;;
 	void)
-	    echo "    $fname(${args%, });"
+	    fdef[$subco]+=$'\n'"    $fname(${args%, });"
 	    ;;
 	*)
-	    echo "/* TODO: returns $fret */"
+	    fdef[$subco]+=$'\n'"/* TODO: returns $fret */"
 	;;
     esac
-    echo "    return 0;"
-    echo "}"
+    fdef[$subco]+=$'\n'"    return 0;"
+    fdef[$subco]+=$'\n'"}"
+    fdef[$subco]+=$'\n'
+    fdef[$subco]+=$'\n'
     #echo >&2 "Fret: $fret"
 
 }
@@ -92,6 +128,17 @@ while read -a def; do
     fi
 done < <(extract_fn /usr/include/newt.h)
 
+printf "%s\n" "${fdecls[@]}"
+
+for so in "${!subcommands[@]}";do
+    echo
+    echo "static const char * ${so^^}_USAGE =    \\"
+    echo '    "usage:"      \'
+    echo "${usage[$so]}" | awk 'NF{printf "    \"%s\"  \\\n", $0}' | sort
+    echo "${fdecls[$so]}"
+    echo "${fdef[$so]}"
+done
+
 # <<EOF 
 # void newtCheckboxSetFlags ( newtComponent co , int flags , enum newtFlagsSense sense ) ;
 # newtComponent newtCompactButton ( int left ,  int top ,  const char * text ) ;
@@ -103,7 +150,8 @@ done < <(extract_fn /usr/include/newt.h)
 
 # EOF
 
-printf "%s\n" "${fdecls[@]}"
+
+
 
 exit
 
