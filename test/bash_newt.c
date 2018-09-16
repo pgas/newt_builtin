369257d944e37213b891e04275e402c4771de1bc
pgas <pgas@192.168.2.10>
2018-09-16 22:14:51 +0300

    figured out --wrap and mocking

---
+++ test/bash_newt.c
@@ -0,0 +1,48 @@
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <cmocka.h>
+#include <stdio.h>
+
+
+#include "bash_newt.h"
+/* /\* a structure which represents a word. *\/ */
+/* typedef struct word_desc { */
+/*   char *word;		/\* Zero terminated string. *\/ */
+/*   int flags;		/\* Flags associated with this word. *\/ */
+/* } WORD_DESC; */
+
+/* /\* A linked list of words. *\/ */
+/* typedef struct word_list { */
+/*   struct word_list *next; */
+/*   WORD_DESC *word; */
+/* } WORD_LIST; */
+
+
+/** make a WORD list with word as root->next **/
+
+void  __wrap_newtInit(){
+  function_called();
+}
+
+static int setup(void **state) {
+  return 0;
+}
+static int teardown(void **state) {
+  return 0;
+}
+
+void test_init_is_called(void **state) {
+  expect_function_call(__wrap_newtInit); 
+  bash_newtInit(NULL); 
+
+}
+
+int main(void)
+{
+  const struct CMUnitTest tests[] = {
+   cmocka_unit_test_setup_teardown(test_init_is_called, setup, teardown),
+  };
+
+  return cmocka_run_group_tests(tests, NULL, NULL);
+}
--- test/type_conversion.c
+++ test/type_conversion.c
@@ -7,20 +7,6 @@
 
 #include "type_conversion.h"
 
-/* /\* a structure which represents a word. *\/ */
-/* typedef struct word_desc { */
-/*   char *word;		/\* Zero terminated string. *\/ */
-/*   int flags;		/\* Flags associated with this word. *\/ */
-/* } WORD_DESC; */
-
-/* /\* A linked list of words. *\/ */
-/* typedef struct word_list { */
-/*   struct word_list *next; */
-/*   WORD_DESC *word; */
-/* } WORD_LIST; */
-
-
-/** make a WORD list with word as root->next **/
 
 static int setup(void **state) {
   return 0;
